d286c65e8d393ff9b40493e6966a91144640e86d
Елисей Угримов <ugrimov35335@gmail.com>
2025-05-20 19:44:29 +0200

    Add working version without .env file

--- database.js
+++ database.js
@@ -1,24 +1,23 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 let database;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect(
-    "mongodb+srv://ugrimov35335:A3mi6TU3x1EJlsZK@cluster0.usnwarh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-    .then((client) => {
-      console.log("Connected to MongoDB!");
-      database = client.db("shop");
-      callback();
-    })
-    .catch((error) => console.log("Connection error:", error));
+const mongoConnect = async (callback) => {
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
+
+  try {
+    const client = await MongoClient.connect(uri);
+    console.log("Connected to MongoDB!");
+    database = client.db("shop");
+    callback();
+  } catch (error) {
+    console.log("Connection error:", error);
+  }
 };
 
 const getDatabase = () => {
-  if (!database) {
-    throw "No database found!";
-  }
+  if (!database) throw "No database found!";
   return database;
 };
 
